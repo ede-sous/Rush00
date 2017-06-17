212ef305eb0f562b460bf8181871ad2cffd2d6c3
Gaspard SCHAETZ <gschaetz@e2r5p8.42.fr>
2017-06-17 22:38:59 +0200

    clarification code index.php

--- home/index.php
+++ home/index.php
@@ -5,88 +5,50 @@ include("../functions/auth.php");
 $sql = db_ini();
 if (auth($_SESSION['login'], $_SESSION['passwd'], $sql) == false)
 {
-	echo '<html>
-			<head>
-				<meta charset="utf-8"/>
-				<title>Inst3grames</title>
-				<link rel="stylesheet" href="../css/index.css"/>
-			</head>
-			<ul id="menu">
-			<li>
-				<a href="../home/index.php">accueil</a>
-			</li>
-			<li>
-				<a href="#">nos bieres</a>
-				<ul>
-					<li><a href="#">blondes</a></li>
-					<li><a href="#">brunes</a></li>
-					<li><a href="#">ambrees</a></li>
-					<li><a href="#">fortes</a></li>
-					<li><a href="#">legeres</a></li>
-					<li><a href="#">classique</a></li>
-				</ul>
-			</li>
-			<li>
-				<a href="#">panier</a>
-			</li>
-			<li>
-				<a href="../user/creation.php">Creer un compte</a>
-			</li>
-			<li>
-				<a href="../user/login_one.php">Se connecter</a>
-			</li>
-		</ul>';
-
-		//'<html>
-	//	<meta charset="utf-8"/>
-	//	<body>
-	//	<button ><a href="../user/creation.php">Créer un compte.</a></button><br />
-	//	<button ><a href="../user/login_one.php">Se connecter.</a></button><br />
-	//	</body>
-	//	</html>';
+	$link1 = "../user/creation.php";
+	$link2 = "../user/login_one.php";
+	$phr1 = "Creer compte";
+	$phr2 = "Se connecter";
 }
 else
 {
-	echo '<html>
-			<head>
-				<meta charset="utf-8"/>
-				<title>inst3grames</title>
-				<link rel="stylesheet" href="../css/index.css"/>
-			</head>
-			<ul id="menu">
-			<li>
-				<a href="../home/index.php">accueil</a>
-			</li>
-			<li>
-				<a href="#">nos bieres</a>
-				<ul>
-					<li><a href="#">blondes</a></li>
-					<li><a href="#">brunes</a></li>
-					<li><a href="#">ambrees</a></li>
-					<li><a href="#">fortes</a></li>
-					<li><a href="#">legeres</a></li>
-					<li><a href="#">classique</a></li>
-				</ul>
-			</li>
-			<li>
-				<a href="#">panier</a>
-			</li>
-			<li>
-				<a href="../user/logout.php">Deconnecter</a>
-			</li>
-			<li>
-				<a href="../user/modificate.php">Modifer le mot de passe</a>
-			</li>
-		</ul>';
-		
-		
-		//'<html>
-		//<meta charset="utf-8"/>
-		//<body>
-		//<button ><a href="../user/logout.php">Se Déconecter.</a></button>
-		//<button ><a href="../user/modificate.php">Modifier le mot de passe.</a></button>
-		//</body>
-		//</html>';
+	$link1 = "../user/logout.php";
+	$link2 = "../user/modificate.php";
+	$phr1 = "Se deconnecter";
+	$phr2 = "Modifier le mot de passe";
 }
 ?>
-
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Inst3grames</title>
+		<link rel="stylesheet" href="../css/index.css"/>
+	</head>
+	<header>
+		<ul id="menu">
+		<li>
+		<a href="../home/index.php">accueil</a>
+		</li>
+		<li>
+			<a href="#">nos bieres</a>
+			<ul>
+				<li><a href="#">blondes</a></li>
+				<li><a href="#">brunes</a></li>
+				<li><a href="#">ambrees</a></li>
+				<li><a href="#">fortes</a></li>
+				<li><a href="#">legeres</a></li>
+				<li><a href="#">classique</a></li>
+			</ul>
+		</li>
+		<li>
+			<a href="#">panier</a>
+		</li>
+		<li>
+		<a href="<?PHP echo $link1;?>"><?PHP echo $phr1; ?></a>
+		</li>
+		<li>
+		<a href="<?PHP echo $link2; ?>"><?PHP echo $phr2; ?></a>
+		</li>
+		</ul>
+	</header>
+</html>
