903b54c35fef286bdfa3b091404d24a636b21d25
Gaspard SCHAETZ <gschaetz@e2r5p8.42.fr>
2017-06-17 21:18:39 +0200

    index.php fusion

--- home/index.php
+++ home/index.php
@@ -1,7 +1,27 @@
-<html>
+<?PHP
+session_start();
+include("../user/db.php");
+include("../user/auth.php");
+$sql = db_ini();
+if (auth($_SESSION['login'], $_SESSION['passwd'], $sql) == false)
+{
+	echo '<html>
 <meta charset="utf-8"/>
 	<body>
 		<button ><a href="../user/creation.php">Créer un compte.</a></button><br />
 		<button ><a href="../user/login_one.php">Se connecter.</a></button><br />
 	</body>
-</html>
+</html>';
+}
+else
+{
+	echo '<html>
+	<meta charset="utf-8"/>
+	<body>
+		<button ><a href="../user/logout.php">Se Déconecter.</a></button>
+		<button ><a href="../user/modificate.php">Modifier le mot de passe.</a></button>
+	</body>
+</html>';
+}
+?>
+
