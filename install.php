d7fd8a810eabfec794d1d4abb2f3f8c52e321624
Gaspard SCHAETZ <gschaetz@e2r5p8.42.fr>
2017-06-17 17:08:09 +0200

    ajous table products

--- install.php
+++ install.php
@@ -4,6 +4,8 @@ $sql = mysqli_connect("localhost", "root", "superpass");
 
 if (!mysqli_query($sql, "CREATE DATABASE edegsc"))
 	return (print("Databases and Tables already installed.\n"));
+
+
 mysqli_select_db($sql, "edegsc");
 
 if (!(mysqli_query($sql, "CREATE TABLE Users (
@@ -14,9 +16,22 @@ if (!(mysqli_query($sql, "CREATE TABLE Users (
 	PRIMARY KEY (id));")))
 	return (print("Problem creating the table.\n"));
 
+if (!(mysqli_query($sql, "CREATE TABLE products (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	color VARCHAR(255) NOT NULL,
+	cereals VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id));")))
+	return (print("Problem creating the table.\n"));
+
+if (!(mysqli_query($sql,"INSERT INTO `products` (`id`, `name`, `color`, `cereals`) VALUES (NULL,'chimey','blonde', 'houblon')")))
+	return (print("Problem creating beer.\n"));
+
 if (!(mysqli_query($sql,"INSERT INTO `Users` (`id`, `login`, `passwd`, `admin`) VALUES (NULL,'ede-sous','".hash("whirlpool", "patate")."', '1')")))
 	return (print("Problem creating the admin.\n"));
 if (!(mysqli_query($sql,"INSERT INTO `Users` (`id`, `login`, `passwd`, `admin`) VALUES (NULL,'gschaetz','".hash("whirlpool", "gaspard2")."', '1')")))
 	return (print("Problem creating the admin.\n"));
+
+
 mysqli_close($sql);
 ?>
