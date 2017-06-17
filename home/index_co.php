1740a0feafb8542c2060e64c3914860230b55f16
Gaspard SCHAETZ <gschaetz@e2r5p8.42.fr>
2017-06-17 19:44:15 +0200

    nodif en php des index

--- home/index_co.php
+++ home/index_co.php
@@ -5,5 +5,11 @@ if (auth($_SESSION['login'], $_SESSION['passwd']) == false)
 	return ;
 }
 echo "passe\n";
-
 ?>
+<html>
+<meta charset="utf-8"/>
+	<body>
+		<button ><a href="../user/logout.php">se déconecter</a></button>
+		<button ><a href="../user/modificate.php">modifier mot de passe</a></button>
+	</body>
+</html>
