77c74b859daff1e5aab219c7c58b9de421292063
Gaspard SCHAETZ <gschaetz@e2r5p5.42.fr>
2017-06-18 17:44:53 +0200

    ajous painer database

--- user/panier.php
+++ user/panier.php
@@ -2,17 +2,14 @@
 session_start();
 include("../functions/db.php");
 $sql = db_ini();
-//print_r($_SESSION['id_prod']);
 if ($_SESSION['id_prod'])
 {
 	$tab_id = array_count_values($_SESSION['id_prod']);
-	//print_r($tab_id);
 	foreach ($tab_id as $i => $elem)
 	{
 		if (!($qry[] = mysqli_fetch_assoc($query = mysqli_query($sql, 'SELECT * FROM `products` WHERE id="'.$i.'"'))));
 	}
 }
-print_r($qry);
 if ($qry != NULL)
 {
 	foreach ($qry as $elem)
@@ -43,6 +40,5 @@ if ($qry != NULL)
 	</form>
 </html>
 <?PHP
-	print_r($buy);
 }
 ?>
